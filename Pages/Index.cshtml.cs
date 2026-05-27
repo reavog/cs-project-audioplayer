07aa084d4cced5f14efb2d33e4ec40325a3eb838
Miguel <reavog23@gmail.com>
2026-05-27 12:45:22 -0500

    Added initial baseline html for the UI.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,23 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using AudioPlayer.Data;
+using AudioPlayer.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace AudioPlayer.Pages;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
+    private readonly AudioPlayerDBContext _context;
+    public IndexModel(AudioPlayerDBContext context)
     {
+        _context = context;
+    }
 
+    public List<Song> Songs { get; set; } = new();
+
+    public async Task OnGetAsync()
+    {
+        Songs = await _context.Songs.ToListAsync();
     }
 }
