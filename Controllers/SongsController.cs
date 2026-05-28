06880c043cae26ab6a1d316e63c7a341b5014122
Miguel <reavog23@gmail.com>
2026-05-27 22:04:00 -0500

    Added a song player controller. The forward button isn't working because I haven't implemented queue method yet.

--- Controllers/SongsController.cs
+++ Controllers/SongsController.cs
@@ -7,27 +7,34 @@ namespace AudioPlayer.Controllers;
 
 [Route("audio")]
 [ApiController]
-public class SongsController : ControllerBase
+public class SongsController(AudioPlayerDBContext context) : ControllerBase
 {
-    private readonly AudioPlayerDBContext _context;
 
-    public SongsController(AudioPlayerDBContext context)
-    {
-        _context = context;
-    }
 
-    [HttpGet]
-    public async Task<ActionResult<List<Song>>> GetAll()
-    {
-        return await _context.Songs.ToListAsync();
-    }
+  [HttpGet]
+  public async Task<ActionResult<List<Song>>> GetAll()
+  {
+    return await context.Songs.ToListAsync();
+  }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Song>> GetbyId(int id)
-    {
-        var song = await _context.Songs.FindAsync(id);
-        if (song == null)
-            return NotFound();
-        return song;
-    }
+  [HttpGet("{id}")]
+  public async Task<ActionResult<Song>> GetbyId(int id)
+  {
+    var song = await context.Songs.FindAsync(id);
+    if (song == null)
+      return NotFound();
+    return song;
+  }
+  [HttpGet("{id}/stream")]
+  public async Task<IActionResult> Stream(int id)
+  {
+    var song = await context.Songs.FindAsync(id);
+    if (song == null)
+      return NotFound();
+
+    var filePath = Path.Combine("home/miguel/Music", song.FilePath);
+    var stream = System.IO.File.OpenRead(filePath);
+
+    return File(stream, "audio/mpeg", enableRangeProcessing: true);
+  }
 }
